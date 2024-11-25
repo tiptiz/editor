1f0575a23df9b4dc75bc40c652d4d33020067294
aolyang <aolyang@foxmail.com>
2024-11-26 01:32:27 +0800

    update: text style icons

---
+++ example/src/components/ui/dropdown-menu/dropdown-menu-content.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { cn, flyAndScale } from "@/utils/shadcn.js"
+
+	import { DropdownMenu as DropdownMenuPrimitive } from "bits-ui"
+
+	type $$Props = DropdownMenuPrimitive.ContentProps
+	type $$Events = DropdownMenuPrimitive.ContentEvents
+
+	let className: $$Props["class"] = undefined
+	export let sideOffset: $$Props["sideOffset"] = 4
+	export let transition: $$Props["transition"] = flyAndScale
+	export let transitionConfig: $$Props["transitionConfig"] = undefined
+	export { className as class }
+</script>
+
+<DropdownMenuPrimitive.Content
+	{transition}
+	{transitionConfig}
+	{sideOffset}
+	class={cn(
+	    "bg-popover text-popover-foreground z-50 min-w-[8rem] rounded-md border p-1 shadow-md focus:outline-none",
+	    className
+	)}
+	{...$$restProps}
+	on:keydown
+>
+	<slot />
+</DropdownMenuPrimitive.Content>
---
+++ example/src/components/ui/dropdown-menu/dropdown-menu-separator.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { cn } from "@/utils/shadcn.js"
+
+	import { DropdownMenu as DropdownMenuPrimitive } from "bits-ui"
+
+	type $$Props = DropdownMenuPrimitive.SeparatorProps
+
+	let className: $$Props["class"] = undefined
+	export { className as class }
+</script>
+
+<DropdownMenuPrimitive.Separator
+	class={cn("bg-muted -mx-1 my-1 h-px", className)}
+	{...$$restProps}
+/>
