4b05b4d0b03da79eea1863d7fb21d5acb57abe8d
aolyang <aolyang@foxmail.com>
2024-12-03 16:10:34 +0800

    update: component Tooltip.svelte

---
+++ example/src/components/Tooltip.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip"
+
+    export let label: string
+</script>
+
+<Tooltip openDelay={100} closeDelay={0}>
+    <TooltipTrigger class="flex">
+        <slot/>
+    </TooltipTrigger>
+    <TooltipContent side="bottom">
+        {#if $$slots.label}
+            <slot name="label"/>
+        {:else}
+            <span>{label}</span>
+        {/if}
+    </TooltipContent>
+</Tooltip>
