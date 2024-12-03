31bab420c6de3711b33af28c8d209e1527377ca5
aolyang <aolyang@foxmail.com>
2024-12-03 16:12:07 +0800

    update: fontSize menu

---
+++ example/src/components/SelectFontSize.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    import AutoComplete, { type SelectOption } from "@/components/AutoComplete.svelte"
+    import EditorTransactionEvent from "@/components/EditorTransactionEvent.svelte"
+    import Tooltip from "@/components/Tooltip.svelte"
+    import { getEditorContext } from "@/states/toolbar"
+    import { css } from "@/utils/config"
+
+    const ctx = getEditorContext()
+    const sizes = [12, 14, 15, 16, 17, 18, 20, 22, 24, 26]
+    const items: SelectOption[] = sizes.map((size) => {
+        const label = `${size}px`
+        return { label, value: label }
+    })
+
+    const handleSelect = (item: SelectOption) => {
+        ctx.editor.chain().focus().setFontSize(item.value).run()
+    }
+    let currentFontSize: string
+    const handleFontSizeTr = () => {
+        currentFontSize = ctx.editor.getAttributes("textStyle")?.fontSize
+    }
+</script>
+
+{#snippet label(item: SelectOption)}
+    <p style={css`
+            font-size: ${item.label};
+            color: ${currentFontSize === item.label ? "hsl(var(--primary))" : "inherit"}
+       `}>
+        {item.label}
+    </p>
+{/snippet}
+<EditorTransactionEvent handler={handleFontSizeTr}/>
+<Tooltip label="select font-size">
+    <AutoComplete items={items} {label} {handleSelect} placeholder={currentFontSize || "default"}/>
+</Tooltip>
