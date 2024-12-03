6e1102f1b297b50bc174fabe8d99b5eaf6a58c61
aolyang <aolyang@foxmail.com>
2024-12-03 17:00:59 +0800

    chore: types error

--- example/src/components/ToolbarTable.svelte
+++ example/src/components/ToolbarTable.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
     import TableCreateNew from "@/components/TableCreateNew.svelte"
     import TableInsertNew from "@/components/TableInsertNew.svelte"
-    import { getEditorContext } from "@/states/toolbar"
-
-    const ctx = getEditorContext()
 </script>
 
 <TableCreateNew/>
