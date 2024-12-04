d9472bdab1ba5f9ec8127b3a26b30c2f85aa62c6
aolyang <aolyang@foxmail.com>
2024-12-04 23:45:44 +0800

    update: hr

--- example/src/components/toolbars/ToolbarOthers.svelte
+++ example/src/components/toolbars/ToolbarOthers.svelte
@@ -3,15 +3,18 @@
     import { getEditorContext } from "@/states/toolbar"
 
     import SvgBlockquote from "@/icons/toolbars/SvgBlockquote.svelte"
+    import SvgHr from "@/icons/toolbars/SvgHr.svelte"
 
     const ctx = getEditorContext()
-    const blockquote = () => {
-        ctx.editor.chain().focus().toggleBlockquote().run()
-    }
+    const blockquote = () => ctx.editor.chain().focus().toggleBlockquote().run()
+    const setHr = () => ctx.editor.chain().focus().setHorizontalRule().run()
 </script>
 
 <div class="ml-[5px] flex items-center gap-x-[13px]">
     <Tooltip label="lift list item">
-        <SvgBlockquote class={ctx.isBlockquote ? "active" : ""} onclick={blockquote} />
+        <SvgBlockquote class={ctx.isBlockquote ? "active" : ""} onclick={blockquote}/>
+    </Tooltip>
+    <Tooltip label="set horizontal rule">
+        <SvgHr onclick={setHr}/>
     </Tooltip>
 </div>
