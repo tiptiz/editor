60013877dd7b9f508d91751726eac70949c201e9
aolyang <aolyang@foxmail.com>
2024-12-04 11:54:19 +0800

    update: toggle table header

--- example/src/components/toolbars/ToolbarTable.svelte
+++ example/src/components/toolbars/ToolbarTable.svelte
@@ -1,11 +1,34 @@
 <script lang="ts">
+    import Tooltip from "@/components/Tooltip.svelte"
+    import { getEditorContext } from "@/states/toolbar"
+
+    import SvgTableHead from "@/icons/toolbars/SvgTableHead.svelte"
     import TableCellMerge from "@/components/toolbars/TableCellMerge.svelte"
     import TableCellSplit from "@/components/toolbars/TableCellSplit.svelte"
     import TableCreateNew from "@/components/toolbars/TableCreateNew.svelte"
     import TableInsertNew from "@/components/toolbars/TableInsertNew.svelte"
+
+    const ctx = getEditorContext()
+
+    const positions = ["left", "top"] as const
+    type Position = typeof positions[number]
+
+    const handleToggleHeader = (position: Position) => {
+        const chain = ctx.editor.chain().focus()
+        if (position === "left") chain.toggleHeaderColumn().run()
+        else if (position === "top") chain.toggleHeaderRow().run()
+    }
 </script>
 
 <TableCreateNew/>
 <TableInsertNew/>
-<TableCellMerge />
-<TableCellSplit />
+<TableCellMerge/>
+<TableCellSplit/>
+{#snippet header(position: Position)}
+    <Tooltip label={`toggle table header ${position}`}>
+        <SvgTableHead position={position} width="16" height="16" onclick={() => handleToggleHeader(position)}/>
+    </Tooltip>
+{/snippet}
+{#each positions as pos}
+    {@render header(pos)}
+{/each}
