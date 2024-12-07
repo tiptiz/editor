2361fee33d190c4edca2c122b1d98246324ad948
aolyang <aolyang@foxmail.com>
2024-12-07 18:04:44 +0800

    update: style toggle locale

--- example/src/components/ToggleLocale.svelte
+++ example/src/components/ToggleLocale.svelte
@@ -5,15 +5,11 @@
     import { locale, locales, t } from "@/utils/i18n"
 
     const toggleLocale = () => {
-        locale.set(
-            $locale === locales[0]
-                ? locales[1]
-                : locales[0]
-        )
+        locale.set($locale === locales[0] ? locales[1] : locales[0])
     }
 </script>
 
-<Button class="py-0 px-2 h-[26px] rounded" on:click={toggleLocale}>
+<Button class="py-0 px-2 h-[26px] rounded" variant="secondary" on:click={toggleLocale}>
     <SvgI18n width="20" height="20"/>
     <div class="w-16">{$t($locale)}</div>
 </Button>
