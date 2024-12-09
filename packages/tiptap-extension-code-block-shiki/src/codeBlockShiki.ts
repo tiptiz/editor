f1c012e9f0178190f42a5675c4ad39dd984e1eff
aolyang <aolyang@foxmail.com>
2024-12-09 13:59:22 +0800

    update: Mod-a keymap codes selection

--- packages/tiptap-extension-code-block-shiki/src/codeBlockShiki.ts
+++ packages/tiptap-extension-code-block-shiki/src/codeBlockShiki.ts
@@ -1,12 +1,23 @@
+import type { Command } from "@tiptap/core"
 import type { CodeBlockOptions } from "@tiptap/extension-code-block"
 import type { PluginShikiOptions } from "./proseMirrorShikiView"
 
 import { proseMirrorPluginShiki } from "./proseMirrorPluginShiki"
 
+import { findParentNode } from "@tiptap/core"
 import CodeBlock from "@tiptap/extension-code-block"
+import { TextSelection } from "@tiptap/pm/state"
 
 export type CodeBlockShikiOptions = CodeBlockOptions & Omit<PluginShikiOptions, "name"> & {
     showLineNumbers: boolean
+}
+
+declare module "@tiptap/core" {
+    interface Commands<ReturnType> {
+        codeBlockShiki: {
+            selectCodes: () => ReturnType
+        }
+    }
 }
 
 export const CodeBlockShiki = CodeBlock.extend<CodeBlockShikiOptions>({
@@ -32,6 +43,29 @@ export const CodeBlockShiki = CodeBlock.extend<CodeBlockShikiOptions>({
                     }
                 }
             }
+        }
+    },
+
+    addCommands() {
+        const chainCommandSelectCodes: () => Command = () => ({ state, tr, dispatch }) => {
+            const { selection } = state
+            const codeBlock = findParentNode(node => node.type.name === this.type.name)(selection)
+            if (codeBlock) {
+                const codesSelection = TextSelection.create(tr.doc, codeBlock.pos + 1, codeBlock.pos + codeBlock.node.nodeSize - 1)
+                tr.setSelection(codesSelection)
+                dispatch(tr)
+                return true
+            }
+            return false
+        }
+        return {
+            selectCodes: chainCommandSelectCodes
+        }
+    },
+
+    addKeyboardShortcuts() {
+        return {
+            "Mod-a": () => this.editor.commands.selectCodes()
         }
     },
 
