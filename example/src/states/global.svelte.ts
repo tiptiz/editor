2539069ee4fa8b6a4e17365d42b58973be9e55ab
aolyang <aolyang@foxmail.com>
2024-12-13 22:39:31 +0800

    chore: update default value

--- example/src/states/global.svelte.ts
+++ example/src/states/global.svelte.ts
@@ -3,16 +3,23 @@
 const True = "!"
 const False = " "
 
-export const globalState = $state({
-    viewSparkLines: localStorage.getItem("toolbar-spark-line") === True,
-    viewToc: localStorage.getItem("toolbar-view-toc") === True,
-    viewAssetsTree: localStorage.getItem("toolbar-view-assets-tree") === True,
-    currentAsset: localStorage.getItem("current-asset")
-})
-
 export const saveState = (key: string, value: boolean | string) => {
     localStorage.setItem(key, typeof value === "string"
         ? value
         : value ? True : False
     )
 }
+
+export const getState = (key: string, defaultValue?: boolean | string) => {
+    const value = localStorage.getItem(key)
+    return value === null
+        ? defaultValue
+        : value === True
+}
+
+export const globalState = $state({
+    viewSparkLines: getState("toolbar-spark-line", True),
+    viewToc: getState("toolbar-view-toc", True),
+    viewAssetsTree: getState("toolbar-view-assets-tree", True),
+    currentAsset: getState("current-asset") || "features.html"
+})
