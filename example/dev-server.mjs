1d8ff63b4da605fcf2692af1762e7f44b6aab319
aolyang <aolyang@foxmail.com>
2024-12-15 00:23:48 +0800

    feat: simple swap file

--- example/dev-server.mjs
+++ example/dev-server.mjs
@@ -12,14 +12,35 @@ const content = express.Router()
 /** @param target {string} */
 const fileTarget = target => path.resolve(__dirname, "./src/assets", target)
 
+/** @param target {string} */
+const fileSwapTarget = (target) => {
+    let swap = target
+    if (!target.endsWith(".swap.html")) swap = target.replace(".html", ".swap.html")
+    return swap
+}
+
+/** @param target {string} */
+const promiseSwapFile = (target) => {
+    let swap = fileSwapTarget(target)
+    fs.copyFileSync(target, swap)
+    return swap
+}
+
 content.get("/content", (req, res) => {
-    res.sendFile(fileTarget(req.query.filepath))
+    let target = fileTarget(req.query.filepath)
+    let swap = fileSwapTarget(target)
+
+    if (fs.existsSync(swap)) res.sendFile(swap)
+    else res.sendFile(target)
 })
 content.put("/content", (req, res) => {
-    const target = req.headers["content-file-path"]
-    if (!target) res.status(404).send("Failed!filepath not found")
+    let relativePath = req.headers["content-file-path"]
+    if (relativePath instanceof Array && relativePath.length) relativePath = relativePath[0]
+    if (!relativePath) res.status(404).send("Failed! filepath not found")
 
-    const stream = fs.createWriteStream(fileTarget(target))
+    const swap = promiseSwapFile(fileTarget(relativePath))
+
+    const stream = fs.createWriteStream(swap)
     req.pipe(stream)
     stream.once("error", () => res.status(500).send("error"))
     stream.once("finish", () => res.status(200).send("done"))
