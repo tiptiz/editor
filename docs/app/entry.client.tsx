4eb0a6fce6f080e616c3ca4de871105d0b3a49cd
aolyang <aolyang@foxmail.com>
2024-12-24 01:37:40 +0800

    chore: add mui

--- docs/app/entry.client.tsx
+++ docs/app/entry.client.tsx
@@ -1,19 +1,56 @@
-/**
- * By default, Remix will handle hydrating your app on the client for you.
- * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
- * For more information, see https://remix.run/file-conventions/entry.client
- */
-
 import { RemixBrowser } from "@remix-run/react"
 
-import { startTransition, StrictMode } from "react"
-import { hydrateRoot } from "react-dom/client"
+import { CacheProvider } from "@emotion/react"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import * as React from "react"
+import * as ReactDOM from "react-dom/client"
 
-startTransition(() => {
-    hydrateRoot(
-        document,
-        <StrictMode>
-            <RemixBrowser />
-        </StrictMode>
+import ClientStyleContext from "@/src/ClientStyleContext"
+import createEmotionCache from "@/src/emotionCache"
+import theme from "@/src/theme"
+
+interface ClientCacheProviderProps {
+    children: React.ReactNode
+}
+
+function ClientCacheProvider({ children }: ClientCacheProviderProps) {
+    const [cache, setCache] = React.useState(createEmotionCache())
+
+    const clientStyleContextValue = React.useMemo(
+        () => ({
+            reset() {
+                setCache(createEmotionCache())
+            }
+        }),
+        []
     )
-})
+
+    return (
+        <ClientStyleContext.Provider value={clientStyleContextValue}>
+            <CacheProvider value={cache}>{children}</CacheProvider>
+        </ClientStyleContext.Provider>
+    )
+}
+
+const hydrate = () => {
+    React.startTransition(() => {
+        ReactDOM.hydrateRoot(
+            document,
+            <ClientCacheProvider>
+                <ThemeProvider theme={theme}>
+                    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                    <CssBaseline />
+                    <RemixBrowser />
+                </ThemeProvider>
+            </ClientCacheProvider>
+        )
+    })
+}
+
+if (window.requestIdleCallback) {
+    window.requestIdleCallback(hydrate)
+} else {
+    // Safari doesn't support requestIdleCallback
+    // https://caniuse.com/requestidlecallback
+    setTimeout(hydrate, 1)
+}
---
+++ docs/app/src/theme.ts
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material"
+
+// Create a theme instance.
+const theme = createTheme({
+    cssVariables: true,
+    palette: {
+    }
+})
+
+export default theme
