9f61ae58baec84b9f8c8c85b2a79ba279b47dcc5
aolyang <aolyang@foxmail.com>
2025-01-03 10:32:25 +0800

    fix: tailwind darkMode

--- docs/tailwind.config.ts
+++ docs/tailwind.config.ts
@@ -6,6 +6,6 @@ export default {
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
     ],
-    darkMode: ["variant", "dark (&:where(.dark *))"],
+    darkMode: ["variant", "&:where(.dark *)"],
     important: true
 } satisfies Config
